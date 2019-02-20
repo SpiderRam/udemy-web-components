64fea607ca56126d6160f5b2c7cad4edffb03f10
Carmen Owen <cowen@paymerang.com>
2019-02-20 14:00:04 -0500

    Finished lecture 54

--- tooltip.js
+++ tooltip.js
@@ -22,6 +22,10 @@ class Tooltip extends HTMLElement {
                     box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
                 }
 
+                :host {
+                    position: relative;
+                }
+
                 :host(.important) {
                     background: var(--color-primary);
                     padding: 0.25rem;
@@ -60,7 +64,6 @@ class Tooltip extends HTMLElement {
         this._tooltipIcon = this.shadowRoot.querySelector('span');
         this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-        this.style.position = 'relative';
         this._render();
     }
 
